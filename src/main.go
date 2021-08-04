418b52670028430c21f7be2baa6baa46fc2a48e3
Daniel Svitan <dankosaurus9@gmail.com>
2021-08-04 10:48:23 +0200

    :sparkles: Adds text intro

--- src/main.go
+++ src/main.go
@@ -8,6 +8,15 @@ import (
 )
 
 func main() {
+	// intro
+	fmt.Printf("Welcome to TicTacToe!\n")
+	fmt.Printf("How to play the game:\n")
+	fmt.Printf("    You can use WASD to move your cursor, and ENTER to select\n")
+	fmt.Printf("    You can press \"Q\" to quit the game\n")
+	fmt.Printf("Press any key to start the game!\n\n")
+	input_manager.TakeInput()
+
+	// start
 	fmt.Printf("Please enter symbol you want to use: \n")
 	humanSymbol := string(input_manager.TakeInput())
 	fmt.Printf("Entered %v...\n", humanSymbol)
@@ -21,18 +30,18 @@ func main() {
 	for {
 		var key rune = input_manager.TakeInput()
 
-		if key == 113 {
+		if key == 113 { // q
 			fmt.Printf("Exiting...\n")
 			os.Exit(0)
-		} else if key == 119 {
+		} else if key == 119 { // w
 			f.ChangeSelected(0)
-		} else if key == 100 {
+		} else if key == 100 { // d
 			f.ChangeSelected(1)
-		} else if key == 115 {
+		} else if key == 115 { // s
 			f.ChangeSelected(2)
-		} else if key == 97 {
+		} else if key == 97 { // a
 			f.ChangeSelected(3)
-		} else if key == 13 {
+		} else if key == 13 { // enter
 			f.Select()
 		}
 
